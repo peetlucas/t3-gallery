50e0bde4e0f0a8d41b8c242d65df17eec882ffd0
peetlucas <pshitote@gmail.com>
2024-06-29 22:24:02 +0300

    Integration of database to UI

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -6,47 +6,70 @@ async function main() {
   try {
     const alice = await prisma.user.create({
       data: {
-        name: 'Alice',
-        email: 'alice@example.com',
-        posts: {
-          create: {
-            title: 'Alice\'s first post',
-            imageUrl: 'Content of Alice\'s first post',
-          },
-        },
-      },
-    });
-
-    const bob = await prisma.user.create({
-      data: {
-        name: 'Bob',
-        email: 'bob@example.com',
+        name: "Alice",
+        email: "alice@exam.com",
         posts: {
           create: [
             {
-              title: 'Bob\'s first post',
-              imageUrl: 'Content of Bob\'s first post',
+              title: "Alice first post",
+              imageUrl:
+                "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/3012c68d-d895-4c4d-8e3f-52c3fcd482dc-qorhd7.jpg",
             },
             {
-              title: 'Bob\'s second post',
-              imageUrl: 'Content of Bob\'s second post',
+              title: "Alice second post",
+              imageUrl:
+                "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/91a9c4c6-94a1-4736-9ab5-3b3bb90ddbd8-w2v8ba.jpg",
+            },
+            {
+              title: "Alice third post",
+              imageUrl:
+                "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/feb9d4d3-ab33-40dd-ba2c-6d1f05b29983-i7hufi.jpg",
             },
           ],
         },
       },
     });
 
-    console.log('Seed data has been created.');
+    const bob = await prisma.user.create({
+      data: {
+        name: "Bob",
+        email: "bob@exam.com",
+        posts: {
+          create: [
+            {
+              title: "Bob's first post",
+              imageUrl:
+                "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/c13f418b-7dc8-4ad1-bdb8-7adf0877ee44-kft8eg.jpg",
+            },
+            {
+              title: "Bob's second post",
+              imageUrl:
+                "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/6c154e09-344d-4ec7-b928-df05158dc2b4-pot1wn.jpg",
+            },
+            {
+              title: "Bob's third post",
+              imageUrl:
+                "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/8434335e-c94c-4d7b-85f5-416bd3c3bf19-mcvv0e.jpg",
+            },
+          ],
+        },
+      },
+    });
+
+    console.log("Seed data has been created.");
   } catch (e) {
     console.error(e);
     process.exit(1);
   } finally {
     // Call the function without returning a promise
-    prisma.$disconnect().then(() => {
-      console.log('Disconnected from database');
-    }).catch((e) => {
-      console.error('Error disconnecting from database:', e);
-    });
+    prisma
+      .$disconnect()
+      .then(() => {
+        console.log("Disconnected from database");
+      })
+      .catch((e) => {
+        console.error("Error disconnecting from database:", e);
+      });
   }
 }
 
