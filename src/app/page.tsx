4f87339c562575b024a8781ece29905e8112bced
peetlucas <pshitote@gmail.com>
2024-06-29 21:45:07 +0300

    force-dynamic reload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,25 +9,26 @@ import { getProviders } from "next-auth/react";
 import { db } from "~/server/db";
 import prisma from "../../lib/prisma";
 
+export const dynamic = "force-dynamic";
+
+const mockUrls = [
+  "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/3012c68d-d895-4c4d-8e3f-52c3fcd482dc-qorhd7.jpg",
+  "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/c13f418b-7dc8-4ad1-bdb8-7adf0877ee44-kft8eg.jpg",
+  "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/6c154e09-344d-4ec7-b928-df05158dc2b4-pot1wn.jpg",
+  "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/8434335e-c94c-4d7b-85f5-416bd3c3bf19-mcvv0e.jpg",
+  "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/91a9c4c6-94a1-4736-9ab5-3b3bb90ddbd8-w2v8ba.jpg",
+  "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/feb9d4d3-ab33-40dd-ba2c-6d1f05b29983-i7hufi.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
   const session = await getSession();
   const posts = await prisma.post.findMany();
-  console.log(posts);
-
-  const mockUrls = [
-    "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/3012c68d-d895-4c4d-8e3f-52c3fcd482dc-qorhd7.jpg",
-    "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/c13f418b-7dc8-4ad1-bdb8-7adf0877ee44-kft8eg.jpg",
-    "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/6c154e09-344d-4ec7-b928-df05158dc2b4-pot1wn.jpg",
-    "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/8434335e-c94c-4d7b-85f5-416bd3c3bf19-mcvv0e.jpg",
-    "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/91a9c4c6-94a1-4736-9ab5-3b3bb90ddbd8-w2v8ba.jpg",
-    "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/feb9d4d3-ab33-40dd-ba2c-6d1f05b29983-i7hufi.jpg",
-  ];
-
-  const mockImages = mockUrls.map((url, index) => ({
-    id: index + 1,
-    url,
-  }));
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
