810e44a64b0761673a557880279b368804503eea
peetlucas <pshitote@gmail.com>
2024-06-29 23:31:50 +0300

    use auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,24 +6,34 @@ import { getSession } from "next-auth/react";
 import SessionProviderWrapper from "./components/SessionProviderWrapper";
 import InteractiveButton from "./components/InteractiveButton";
 import { getProviders } from "next-auth/react";
+import Image from "next/image";
 import { db } from "~/server/db";
 import prisma from "../../lib/prisma";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-  const session = await getSession();
+  // const hello = await api.post.hello({ text: "from tRPC" });
+  // const session = await getSession();
   const images = await prisma.post.findMany({
     orderBy: { createdAt: "desc" },
   });
+  // const images = await getMyImages();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-wrap items-center justify-center gap-4 px-4 py-16">
-        {images.map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
-            <img src={image.imageUrl} />
+        {images.map((image) => (
+          <div key={image.id} className="flex h-48 w-48 flex-col">
+            <Link href={`/img/${image.id}`}>
+              <Image
+                src={image.imageUrl}
+                style={{ objectFit: "contain" }}
+                width={192}
+                height={192}
+                alt={image.title}
+              />
+            </Link>
             <div>{image.title}</div>
           </div>
         ))}
